8667ff07287c11fb0d31f5428c2ec98250584a7c
hardik900 <85899671+hardik900@users.noreply.github.com>
2024-11-17 23:22:52 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar(){
             <ul className="nav">
                 <li><Link style={{color:"black", textDecoration:"none",  float:"right"}} to="/Cart">Cart</Link></li>
                 <li><Link style={{color:"black", textDecoration:"none"}} to="/">Products</Link></li>
-                <li><Link style={{color:"black", textDecoration:"none",  float:"right"}} to="/signup">SignUp</Link></li>
+{/*                 <li><Link style={{color:"black", textDecoration:"none",  float:"right"}} to="/signup">SignUp</Link></li> */}
             </ul>
         </>
     )
